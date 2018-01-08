56d7435fd1ba3bd02825c52f1e5f6b3f24971774
Tom Ng <muchadoabouthamlets@gmail.com>
2018-01-08 16:04:49 -0500

    Added exit and settings buttons to the menu. Added exit functionality and settings activity.

--- app/src/main/java/com/example/yetanotherriichimahjongapp/MainMenuActivity.java
+++ app/src/main/java/com/example/yetanotherriichimahjongapp/MainMenuActivity.java
@@ -1,10 +1,12 @@
 package com.example.yetanotherriichimahjongapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,7 +37,13 @@ public class MainMenuActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            //Open settings
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.action_exit) {
+            //Exit app from main activity
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
