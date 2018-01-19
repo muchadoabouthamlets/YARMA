078be975a453182c7697b89db6f95219fcc6a3e0
Tom Ng <muchadoabouthamlets@gmail.com>
2018-01-19 00:15:19 -0500

    Added more equals() tests. Fixed an otherwise redundant test for equals().

--- app/src/test/java/com/example/yetanotherriichimahjongapp/TileUnitTest.java
+++ app/src/test/java/com/example/yetanotherriichimahjongapp/TileUnitTest.java
@@ -1,6 +1,7 @@
 package com.example.yetanotherriichimahjongapp;
 
 import org.junit.Test;
+
 import mahjong.Tile;
 
 import static org.junit.Assert.*;
@@ -268,7 +269,7 @@ public class TileUnitTest {
                 Tile.Rank.TON, Tile.Rank.NAN, Tile.Rank.SHA, Tile.Rank.PEI
         };
 
-        for (Tile.Rank r: ranks) {
+        for (Tile.Rank r : ranks) {
             Tile tile = new Tile(r, Tile.Suit.SANGENPAI);
             assertEquals(Tile.Rank.UNKNOWN, tile.getRank());
             assertEquals(Tile.Suit.UNKNOWN, tile.getSuit());
@@ -281,7 +282,7 @@ public class TileUnitTest {
                 Tile.Rank.HAKU, Tile.Rank.HATSU, Tile.Rank.CHUN
         };
 
-        for (Tile.Rank r: ranks) {
+        for (Tile.Rank r : ranks) {
             Tile tile = new Tile(r, Tile.Suit.KAZEHAI);
             assertEquals(Tile.Rank.UNKNOWN, tile.getRank());
             assertEquals(Tile.Suit.UNKNOWN, tile.getSuit());
@@ -302,7 +303,7 @@ public class TileUnitTest {
         Tile.Attribute a = Tile.Attribute.AKADORA;
         Tile.Rank ranks[];
 
-        Tile.Suit suits[] = new Tile.Suit[] {
+        Tile.Suit suits[] = new Tile.Suit[]{
                 Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
         };
 
@@ -344,7 +345,7 @@ public class TileUnitTest {
         Tile.Attribute a = Tile.Attribute.VISIBLE_TO_ALL_PLAYERS; //dummy non-akadora attribute
         Tile.Rank ranks[];
 
-        Tile.Suit suits[] = new Tile.Suit[] {
+        Tile.Suit suits[] = new Tile.Suit[]{
                 Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
         };
 
@@ -386,7 +387,7 @@ public class TileUnitTest {
         Tile.Attribute a = Tile.Attribute.AKADORA;
         Tile.Rank ranks[];
 
-        ranks = new Tile.Rank[] {
+        ranks = new Tile.Rank[]{
                 Tile.Rank.TON, Tile.Rank.NAN, Tile.Rank.SHA, Tile.Rank.PEI,
         };
 
@@ -424,7 +425,7 @@ public class TileUnitTest {
     @Test
     public void threeArgumentConstructor_4() throws Exception {
 
-        Tile.Attribute attributes[] = new Tile.Attribute[] {
+        Tile.Attribute attributes[] = new Tile.Attribute[]{
                 Tile.Attribute.VISIBLE_TO_NO_PLAYERS,
                 Tile.Attribute.VISIBLE_TO_OWNER,
                 Tile.Attribute.VISIBLE_TO_ALL_PLAYERS
@@ -461,7 +462,7 @@ public class TileUnitTest {
     @Test
     public void threeArgumentConstructor_5() throws Exception {
 
-        Tile.Attribute attributes[] = new Tile.Attribute[] {
+        Tile.Attribute attributes[] = new Tile.Attribute[]{
                 Tile.Attribute.VISIBLE_TO_NO_PLAYERS,
                 Tile.Attribute.VISIBLE_TO_OWNER,
                 Tile.Attribute.VISIBLE_TO_ALL_PLAYERS,
@@ -492,7 +493,7 @@ public class TileUnitTest {
     @Test
     public void threeArgumentConstructor_6() throws Exception {
 
-        Tile.Attribute attributes[] = new Tile.Attribute[] {
+        Tile.Attribute attributes[] = new Tile.Attribute[]{
                 Tile.Attribute.VISIBLE_TO_NO_PLAYERS,
                 Tile.Attribute.VISIBLE_TO_OWNER,
                 Tile.Attribute.VISIBLE_TO_ALL_PLAYERS,
@@ -502,14 +503,14 @@ public class TileUnitTest {
         for (Tile.Attribute a : attributes) {
             for (Tile.Attribute b : attributes) {
                 if (a != b) {
-                    Tile.Attribute attributesArray[] = new Tile.Attribute[] {a, b};
+                    Tile.Attribute attributesArray[] = new Tile.Attribute[]{a, b};
 
-                    Tile t = new Tile(Tile.Rank.NUM_5, Tile.Suit.MANZU,attributesArray);
-                    assertEquals(Tile.Rank.UNKNOWN,t.getRank());
+                    Tile t = new Tile(Tile.Rank.NUM_5, Tile.Suit.MANZU, attributesArray);
+                    assertEquals(Tile.Rank.UNKNOWN, t.getRank());
                     assertEquals(Tile.Suit.UNKNOWN, t.getSuit());
                     assertEquals(Tile.Type.UNKNOWN, t.getType());
-                    assertEquals(true,t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
-                    assertEquals(false,t.is(Tile.Attribute.AKADORA));
+                    assertEquals(true, t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
+                    assertEquals(false, t.is(Tile.Attribute.AKADORA));
                 }
             }
         }
@@ -519,14 +520,14 @@ public class TileUnitTest {
             for (Tile.Attribute b : attributes) {
                 for (Tile.Attribute c : attributes) {
                     if ((a != b) && (a != c) && (b != c)) {
-                        Tile.Attribute attributesArray[] = new Tile.Attribute[] {a, b, c};
+                        Tile.Attribute attributesArray[] = new Tile.Attribute[]{a, b, c};
 
-                        Tile t = new Tile(Tile.Rank.NUM_5, Tile.Suit.MANZU,attributesArray);
-                        assertEquals(Tile.Rank.UNKNOWN,t.getRank());
+                        Tile t = new Tile(Tile.Rank.NUM_5, Tile.Suit.MANZU, attributesArray);
+                        assertEquals(Tile.Rank.UNKNOWN, t.getRank());
                         assertEquals(Tile.Suit.UNKNOWN, t.getSuit());
                         assertEquals(Tile.Type.UNKNOWN, t.getType());
-                        assertEquals(true,t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
-                        assertEquals(false,t.is(Tile.Attribute.AKADORA));
+                        assertEquals(true, t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
+                        assertEquals(false, t.is(Tile.Attribute.AKADORA));
                     }
                 }
             }
@@ -539,39 +540,43 @@ public class TileUnitTest {
     public void threeArgumentConstructor_7() throws Exception {
 
         Tile.Rank ranks[];
-        ranks = new Tile.Rank[] {
+        ranks = new Tile.Rank[]{
                 Tile.Rank.TON, Tile.Rank.NAN, Tile.Rank.SHA, Tile.Rank.PEI
         };
-        Tile.Attribute attributesArray[] = new Tile.Attribute[] {
+        Tile.Attribute attributes[] = new Tile.Attribute[]{
                 Tile.Attribute.VISIBLE_TO_NO_PLAYERS,
                 Tile.Attribute.VISIBLE_TO_OWNER,
                 Tile.Attribute.VISIBLE_TO_ALL_PLAYERS
         };
 
         for (Tile.Rank r : ranks) {
-            Tile t = new Tile(r, Tile.Suit.KAZEHAI,attributesArray);
-            assertEquals(Tile.Rank.UNKNOWN,t.getRank());
-            assertEquals(Tile.Suit.UNKNOWN, t.getSuit());
-            assertEquals(Tile.Type.UNKNOWN, t.getType());
-            assertEquals(true,t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
-            assertEquals(false,t.is(Tile.Attribute.AKADORA));
+            for (Tile.Attribute a : attributes) {
+                Tile.Attribute attributesArray[] = new Tile.Attribute[]{a, Tile.Attribute.AKADORA};
+                Tile t = new Tile(r, Tile.Suit.KAZEHAI, attributesArray);
+                assertEquals(Tile.Rank.UNKNOWN, t.getRank());
+                assertEquals(Tile.Suit.UNKNOWN, t.getSuit());
+                assertEquals(Tile.Type.UNKNOWN, t.getType());
+                assertEquals(true, t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
+                assertEquals(false, t.is(Tile.Attribute.AKADORA));
+            }
         }
-
         ranks = new Tile.Rank[]{
                 Tile.Rank.HAKU, Tile.Rank.HATSU, Tile.Rank.CHUN
         };
 
         for (Tile.Rank r : ranks) {
-            Tile t = new Tile(r, Tile.Suit.SANGENPAI,attributesArray);
-            assertEquals(Tile.Rank.UNKNOWN,t.getRank());
-            assertEquals(Tile.Suit.UNKNOWN, t.getSuit());
-            assertEquals(Tile.Type.UNKNOWN, t.getType());
-            assertEquals(true,t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
-            assertEquals(false,t.is(Tile.Attribute.AKADORA));
+            for (Tile.Attribute a : attributes) {
+                Tile.Attribute attributesArray[] = new Tile.Attribute[]{a, Tile.Attribute.AKADORA};
+                Tile t = new Tile(r, Tile.Suit.SANGENPAI, attributesArray);
+                assertEquals(Tile.Rank.UNKNOWN, t.getRank());
+                assertEquals(Tile.Suit.UNKNOWN, t.getSuit());
+                assertEquals(Tile.Type.UNKNOWN, t.getType());
+                assertEquals(true, t.is(Tile.Attribute.VISIBLE_TO_NO_PLAYERS));
+                assertEquals(false, t.is(Tile.Attribute.AKADORA));
+            }
         }
 
     }
-
 
 
     /*
@@ -584,25 +589,25 @@ public class TileUnitTest {
         Tile.Rank ranks[];
         Tile.Suit suits[];
 
-        ranks= new Tile.Rank[] {
+        ranks = new Tile.Rank[]{
                 Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
                 Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
                 Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
         };
-        suits = new Tile.Suit[] {Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU};
+        suits = new Tile.Suit[]{Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU};
 
         for (Tile.Rank r : ranks) {
             for (Tile.Suit s : suits) {
-                Tile t1 = new Tile(r,s);
-                Tile t2 = new Tile(r,s);
+                Tile t1 = new Tile(r, s);
+                Tile t2 = new Tile(r, s);
                 assertEquals(true, t1.equals(t2));
                 assertEquals(true, t2.equals(t1));
-                assertEquals(t1,t2);
+                assertEquals(t1, t2);
                 assertEquals(t2, t1);
             }
         }
 
-        ranks= new Tile.Rank[] {
+        ranks = new Tile.Rank[]{
                 Tile.Rank.TON, Tile.Rank.NAN, Tile.Rank.SHA, Tile.Rank.PEI,
                 Tile.Rank.HAKU, Tile.Rank.HATSU, Tile.Rank.CHUN
         };
@@ -611,9 +616,232 @@ public class TileUnitTest {
             Tile t2 = new Tile(r);
             assertEquals(true, t1.equals(t2));
             assertEquals(true, t2.equals(t1));
-            assertEquals(t1,t2);
-            assertEquals(t2,t1);
+            assertEquals(t1, t2);
+            assertEquals(t2, t1);
         }
     }
 
+    //Tests 2 tiles, one of which is an akadora and the other is not but
+    //is otherwise equal in rank, suit and type.
+    //should return true if the akadora boolean is false; or false if the akadora boolean is true
+    @Test
+    public void equals_2() throws Exception {
+        Tile.Rank ranks[] = new Tile.Rank[]{
+                Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
+                Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
+                Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
+        };
+        Tile.Suit suits[] = new Tile.Suit[]{
+                Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
+        };
+
+        for (Tile.Rank r : ranks) {
+            for (Tile.Suit s : suits) {
+                Tile t1 = new Tile(r, s);
+                Tile t2 = new Tile(r, s, Tile.Attribute.AKADORA);
+
+                //akadora boolean is false
+                assertEquals(true, t1.equals(t2));
+                assertEquals(true, t2.equals(t1));
+                assertEquals(t1, t2);
+                assertEquals(t2, t1);
+
+                //akadora boolean is true
+                assertNotEquals(true, t1.equals(t2, true));
+                assertNotEquals(true, t2.equals(t1, true));
+
+            }
+        }
+    }
+
+    //Tests 2 tiles both of which have the same rank, suit, type and whether the tile is
+    //an akadora or not but the two tiles have different visibilities.
+    //Should return true if the allAttributes boolean is false; false otherwise
+    @Test
+    public void equals_3() throws Exception {
+        Tile.Rank ranks[] = new Tile.Rank[]{
+                Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
+                Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
+                Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
+        };
+        Tile.Suit suits[] = new Tile.Suit[]{
+                Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
+        };
+        Tile.Attribute attributes[] = new Tile.Attribute[]{
+                Tile.Attribute.VISIBLE_TO_NO_PLAYERS,
+                Tile.Attribute.VISIBLE_TO_OWNER,
+                Tile.Attribute.VISIBLE_TO_OWNER
+        };
+
+        for (Tile.Rank r : ranks) {
+            for (Tile.Suit s : suits) {
+                for (Tile.Attribute a1 : attributes) {
+                    for (Tile.Attribute a2 : attributes) {
+                        if (a1 != a2) {
+                            Tile.Attribute attributesArray1[] = new Tile.Attribute[]{
+                                    Tile.Attribute.AKADORA, a1
+                            };
+                            Tile.Attribute attributesArray2[] = new Tile.Attribute[]{
+                                    Tile.Attribute.AKADORA, a2
+                            };
+
+                            Tile t1 = new Tile(r, s, attributesArray1);
+                            Tile t2 = new Tile(r, s, attributesArray2);
+
+                            //allAttributes boolean is false
+                            assertEquals(true, t1.equals(t2, true, false));
+                            assertEquals(true, t1.equals(t2, true, false));
+
+                            //allAttributes boolean is true
+                            assertNotEquals(true, t1.equals(t2, true, true));
+                            assertNotEquals(true, t2.equals(t1, true, true));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //Tests 2 tiles, both of which have the same rank, suit, type and attributes
+    //but the attributes are not in the same order.
+    //Should return true on equals() since the order is not taken into consideration
+    @Test
+    public void equals_4() throws Exception {
+        Tile.Rank ranks[] = new Tile.Rank[]{
+                Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
+                Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
+                Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
+        };
+        Tile.Suit suits[] = new Tile.Suit[]{
+                Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
+        };
+        Tile.Attribute attributes[] = new Tile.Attribute[]{
+                Tile.Attribute.VISIBLE_TO_NO_PLAYERS,
+                Tile.Attribute.VISIBLE_TO_OWNER,
+                Tile.Attribute.VISIBLE_TO_OWNER
+        };
+
+        for (Tile.Rank r : ranks) {
+            for (Tile.Suit s : suits) {
+                for (Tile.Attribute a : attributes) {
+                    Tile.Attribute attributesArray1[] = new Tile.Attribute[]{
+                            a, Tile.Attribute.AKADORA
+                    };
+                    Tile.Attribute attributesArray2[] = new Tile.Attribute[]{
+                            Tile.Attribute.AKADORA, a
+                    };
+                    Tile t1 = new Tile(r, s, attributesArray1);
+                    Tile t2 = new Tile(r, s, attributesArray2);
+
+                    assertEquals(true, t1.equals(t2, true, true));
+                    assertEquals(true, t2.equals(t1, true, true));
+                }
+            }
+        }
+    }
+
+    //Tests 2 tiles which have different ranks
+    //Should return false
+    @Test
+    public void equals_5() throws Exception {
+        Tile.Rank ranks[];
+
+        ranks = new Tile.Rank[]{
+                Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
+                Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
+                Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
+        };
+        Tile.Suit suits[] = new Tile.Suit[]{
+                Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
+        };
+
+        for (Tile.Rank r1 : ranks) {
+            for (Tile.Rank r2 : ranks) {
+                for (Tile.Suit s : suits) {
+                    if (r1 != r2) {
+                        Tile t1 = new Tile(r1, s);
+                        Tile t2 = new Tile(r2, s);
+                        assertNotEquals(t1, t2);
+                        assertNotEquals(t2, t1);
+                    }
+                }
+            }
+        }
+
+        ranks = new Tile.Rank[]{
+                Tile.Rank.TON, Tile.Rank.NAN, Tile.Rank.SHA, Tile.Rank.PEI,
+                Tile.Rank.HAKU, Tile.Rank.HATSU, Tile.Rank.CHUN
+        };
+
+        for (Tile.Rank r1 : ranks) {
+            for (Tile.Rank r2 : ranks) {
+                if (r1 != r2) {
+                    Tile t1 = new Tile(r1);
+                    Tile t2 = new Tile(r2);
+                    assertNotEquals(t1, t2);
+                    assertNotEquals(t2, t1);
+                }
+            }
+        }
+    }
+
+    //Tests 2 tiles which have different suits
+    //Should return false
+    @Test
+    public void equals_6() throws Exception {
+
+        Tile.Rank ranks[] = new Tile.Rank[]{
+                Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
+                Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
+                Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
+        };
+        Tile.Suit suits[] = new Tile.Suit[]{
+                Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
+        };
+
+        for (Tile.Rank r : ranks) {
+            for (Tile.Suit s1 : suits) {
+                for (Tile.Suit s2 : suits) {
+                    if (s1 != s2) {
+                        Tile t1 = new Tile(r, s1);
+                        Tile t2 = new Tile(r, s2);
+                        assertNotEquals(t1, t2);
+                        assertNotEquals(t2, t1);
+                    }
+                }
+            }
+        }
+    }
+
+    //Tests 2 tiles which have different types (jihai and nonjihai)
+    //Should return false
+    @Test
+    public void equals_7() throws Exception {
+
+        Tile.Rank ranks1[] = new Tile.Rank[]{
+                Tile.Rank.NUM_1, Tile.Rank.NUM_2, Tile.Rank.NUM_3,
+                Tile.Rank.NUM_4, Tile.Rank.NUM_5, Tile.Rank.NUM_6,
+                Tile.Rank.NUM_7, Tile.Rank.NUM_8, Tile.Rank.NUM_9
+        };
+
+        Tile.Rank ranks2[] = new Tile.Rank[]{
+                Tile.Rank.TON, Tile.Rank.NAN, Tile.Rank.SHA, Tile.Rank.PEI,
+                Tile.Rank.HAKU, Tile.Rank.HATSU, Tile.Rank.CHUN
+        };
+
+        Tile.Suit suits[] = new Tile.Suit[]{
+                Tile.Suit.MANZU, Tile.Suit.PINZU, Tile.Suit.SOUZU
+        };
+
+        for (Tile.Rank r1 : ranks1) {
+            for (Tile.Rank r2 : ranks2) {
+                for (Tile.Suit s : suits) {
+                    Tile t1 = new Tile(r1, s);
+                    Tile t2 = new Tile(r2, s);
+                    assertNotEquals(t1, t2);
+                    assertNotEquals(t2, t1);
+                }
+            }
+        }
+    }
 }
